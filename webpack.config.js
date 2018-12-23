bdf2ebe3fc0c9f9fb8bfffe6d101c195ed52bbd4
mikhailsinyakov <mikhail.sinyakov86@gmail.com>
2018-12-23 14:14:54 +0300

    Update webpack and babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,12 @@
 module.exports = {
-    entry: __dirname + '/app/client/index.js',
+    entry: './app/client/index.js',
     output: {
         path: __dirname + '/public/',
         filename: 'script.js'
     },
     module: {
-        loaders: [{
-            test: /\.js$/,
-            loaders: 'babel-loader',
-            query: {
-                presets: ['react']
-            }
+        rules: [{
+            use: 'babel-loader'
         }]
     }
 };
